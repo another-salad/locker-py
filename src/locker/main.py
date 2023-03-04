b67c44f89b8b39ce3f92f2fa9962828fce9de695
Not Today <48966874+another-salad@users.noreply.github.com>
2023-03-04 14:47:13 +0000

    setup, packages

---
+++ src/locker/main.py
@@ -0,0 +1,12 @@
+
+def encrypt(*args, **kwargs):
+    """Placeholder"""
+    pass
+
+def decrypt(*args, **kwargs):
+    """Placeholder"""
+    pass
+
+def generate():
+    """Placeholder"""
+    pass
