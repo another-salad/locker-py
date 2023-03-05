f84f7bec884a3a7742a186885debd0a1efcca16d
Not Today <48966874+another-salad@users.noreply.github.com>
2023-03-05 01:19:14 +0000

    Take key out of cmdline args

--- src/locker/main.py
+++ src/locker/main.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
+from getpass import getpass
 from src.locker.common.crpyto import encryptor
 
 
@@ -13,7 +14,6 @@ class MainArgs(ArgumentParser):
     def __init__(self, description=_desc):
         super().__init__(description=description)
         self.add_argument("--decrypt", dest="decrypt", action="store_true")
-        self.add_argument("--key", dest="key", type=str)
         self.add_argument("--source", dest="source", type=str)
         self.add_argument("--dest", dest="dest", type=str)
 
@@ -28,9 +28,10 @@ def _path_checker(path_to_check: Path):
         raise PathSadness(f"{path_to_check} isn't a valid path.")
 
 
-def do_action(decrypt: bool, key: str, source: str, dest: str) -> str | None:
+def do_action(decrypt: bool, source: str, dest: str) -> str | None:
     source_path = Path(source)
     _path_checker(source_path)
+    key = getpass("Please enter your key: ")
     if not decrypt:
         return encryptor(key, source_path, Path(dest))
 
@@ -38,4 +39,4 @@ def do_action(decrypt: bool, key: str, source: str, dest: str) -> str | None:
 if __name__ == "__main__":
     """lets parse some args"""
     args, _ = MainArgs().parse_known_args()
-    do_action(args.decrypt, args.key, args.source, args.dest)
+    do_action(args.decrypt, args.source, args.dest)
