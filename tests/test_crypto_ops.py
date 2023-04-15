be024fa34c19439190bcbd13b4dac1656094aaee
Not Today <48966874+another-salad@users.noreply.github.com>
2023-04-15 08:39:33 +0100

    Adding tests

--- tests/test_crypto_ops.py
+++ tests/test_crypto_ops.py
@@ -23,6 +23,7 @@ class CryptoTestCase(TestCase):
     single_file_dir = Path(root_in_dir, "single_file")
     multi_dir = Path(root_in_dir, "multi_dir")
     out_dir = Path(Path(__file__).parent.resolve(), "output")
+    parent_dt_dir = r"\d{4}-\d{2}-\d{2}_\d{2}.\d{2}.\d{2}___r"
 
     @contextmanager
     def _wrapper(self, fname):
@@ -32,14 +33,14 @@ class CryptoTestCase(TestCase):
         yield test_sub_dir
 
     def test_encrypt_content_single_file(self):
-        with self._wrapper(f'test_encrypt_content.{datetime.strftime(datetime.now(), "%H.%M.%S")}') as test_out_dir:
+        with self._wrapper(f'test_encrypt_content_single_file.{datetime.strftime(datetime.now(), "%H.%M.%S")}') as test_out_dir:
             res = encryptor(self.test_key, self.single_file_dir, test_out_dir)
 
         self.assertEqual(res, 0, "Return code should be 0")
         out_files = list(test_out_dir.glob("**/*"))
         self.assertEqual(len(out_files), 3)
         self.assertRegex(
-            out_files[0].parts[-1], r"\d{4}-\d{2}-\d{2}_\d{2}.\d{2}.\d{2}___r", "Expected a timestamped 'root' DIR to be created"
+            out_files[0].parts[-1], self.parent_dt_dir, "Expected a timestamped 'root' DIR to be created"
         )
         self.assertNotEqual(out_files[2].name, "test.txt")
         self.assertNotEqual(out_files[2].read_text("UTF-8"), "super secret data!", "Test file data should be encrypted")
@@ -58,6 +59,22 @@ class CryptoTestCase(TestCase):
         self.assertEqual(len(out_files), 2)
         self.assertEqual(out_files[1].name, "test.txt")
         self.assertEqual(out_files[1].read_text("UTF-8"), "super secret data!", "Test file data should be decrypted")
+
+    def test_encrypt_content_multi_dir(self):
+        with self._wrapper(f'test_encrypt_content_multi_dir.{datetime.strftime(datetime.now(), "%H.%M.%S")}') as test_out_dir:
+            res = encryptor(self.test_key, self.multi_dir, test_out_dir)
+
+        self.assertEqual(res, 0, "Return code should be 0")
+        out_files = list(test_out_dir.glob("**/*"))
+        self.assertEqual(len(out_files), 7)
+        self.assertRegex(
+            out_files[0].parts[-1], self.parent_dt_dir, "Expected a timestamped 'root' DIR to be created"
+        )
+        for f_name in ["even_more_inner", "inner_file", "outer_file"]:
+            for enc_f_path in out_files:
+                self.assertNotIn(f_name, str(enc_f_path), "file names should be encrypted")
+                if enc_f_path.is_file():
+                    self.assertNotIn("secret", enc_f_path.read_text("UTF-8").lower(), "file contents should be encrypted")
 
 
 if __name__ == "__main__":
